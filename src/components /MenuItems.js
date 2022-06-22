0dee37963a1f06783b8f7ec6a18a26e9f482e114
Alston Pough <alstonpough@Alstons-MacBook-Pro.local>
2022-06-22 15:00:13 -0400

    nav

---
+++ src/components /MenuItems.js
@@ -0,0 +1,32 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#home',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Discover',
+        url: '#discover',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Communities',
+        url: '#communities',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Distribution',
+        url: '#distribution',
+        cName: 'nav-links'
+    },
+    {
+        title: 'My Profile',
+        url: '#profile',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Sign Up',
+        url: '#signup',
+        cName: 'nav-links'
+    }
+]
